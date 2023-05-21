3a8af4506819978f0a777a814dc4819a2f5f3faf
Deanna Stevens <deanna.sofias@gmail.com>
2023-05-21 12:27:13 -0400

    Add questions 1 and 2

--- functions.js
+++ functions.js
@@ -4,10 +4,21 @@
 // Use console.log() to see the return value in the console.
 
 // 1: Write a function named greeting that returns a string with a general greeting. 
-
+function greeting(){
+    return "Good morning";
+  }
+  greeting();
+  greeting();
+  
+  console.log(greeting())
 
 // 2: Write a function named customGreeting that returns a greeting WITH a specific name.
 
+function customGreeting(name){
+    return `Good morning, ${name}!`;
+  }
+  console.log(customGreeting("Deanna"));
+  
 
 // 3: Write a function named greetPerson that takes in 3 strings, a first, middle, and last name, and returns a sentence with the full name.
 
